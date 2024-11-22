b36a2b43c378c2fa95b1f62e4f1cee11dd9f2984
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2024-11-22 17:07:53 +0000

    Add missing dependencies (#53)
    * Add missing dependencies

--- src/main/java/org/apache/maven/plugins/issues/IssuesReportHelper.java
+++ src/main/java/org/apache/maven/plugins/issues/IssuesReportHelper.java
@@ -23,7 +23,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.collections.bidimap.DualHashBidiMap;
+import org.apache.commons.collections4.bidimap.DualHashBidiMap;
 import org.apache.maven.plugin.logging.Log;
 
 /**
